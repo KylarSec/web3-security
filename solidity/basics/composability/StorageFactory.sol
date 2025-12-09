c29712e61acb9825005ce5feb4d788ca379d95dd
Sumit Kumar Verma <93787974+KylarSec@users.noreply.github.com>
2025-12-09 19:39:09 +0530

    Shifted Three contract to new file

---
+++ solidity/basics/composability/StorageFactory.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// Import SimpleStorage contract from solidity file.
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+contract StorageFactory {
+
+    // State variable to store the deployed SimpleStorage contract 
+    // Left side is contract type and right side is the variable 
+    SimpleStorage public simpleStorage;
+
+    // Function To deploy a new simplestorage contract
+    function createSimpleStorage() public {
+        // Deploys a new SimpleStorage contract and saves its address in 'simpleStorage'
+        simpleStorage = new SimpleStorage();
+        
+    }
+}
