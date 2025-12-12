54b570be8339d4bd9fea0341b0bb6f8d34c9592c
Sumit Kumar Verma <93787974+KylarSec@users.noreply.github.com>
2025-12-12 20:42:03 +0530

    Added Simple Verison of FundMe

---
+++ learning/solidity/06_Fund_me/FundMe.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// Get Funds from user
+// Withdraw funds
+// set a minimum funding value in USD
+
+contract FundMe {
+
+    // Function to get fund from the user
+    function fund() public payable {
+        // This Function will allow users to send money
+        // Have a miniumum limit to send
+    }
+
+    // Function to Withdraw funds by owner
+    function withdraw() public {}
+
+
+}
