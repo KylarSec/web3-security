3886f4b95ea8459a6fdea3c28b2bda828788ec54
Sumit Kumar Verma <93787974+KylarSec@users.noreply.github.com>
2025-12-13 12:31:56 +0530

    Changed state Variable name from "value to myValue"

--- learning/solidity/0x_playground/ValueExperiment.sol
+++ learning/solidity/0x_playground/ValueExperiment.sol
@@ -2,27 +2,24 @@
 pragma solidity ^0.8.18;
 
 contract Test1 {
-
     // Stores last ETH amount sent (in Wei)
-    uint256 value;
+    uint256 myValue;
 
     // Function can receive ETH
     function pay() public payable {
         // Save the amount of ETH sent
-        value = msg.value;
+        myValue = msg.value;
     }
 
     // Read value in ETH
-    function ReadValue() public view returns(uint256) {
-        
+    function ReadValue() public view returns (uint256) {
         // 1 ETH = 1e18 wei
-        return value / 1e18;
+        return myValue / 1e18;
     }
 
     // A function to read total balance of the contract
-    function ContractBalance() public view returns(uint256) {
-        
-        // Balance of this contract returned in ETH 
+    function ContractBalance() public view returns (uint256) {
+        // Balance of this contract returned in ETH
         return address(this).balance / 1e18; // Converted from wei to ETH
     }
-} 
+}
